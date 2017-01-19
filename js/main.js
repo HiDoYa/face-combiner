08c824cf72c463fae1ee99bc92bd6f37594ad515
HiDoYa <hiroyagojo@gmail.com>
2017-01-18 21:49:36 -0800

    File uploading for single images work. The picture elements are hidden at first, and show when clicked.

--- js/main.js
+++ js/main.js
@@ -1,54 +1,64 @@
-//TODO Allow for multiple file uploads
-//TODO Choose random file 
-//TODO Allow for scrolling through other combinations of faces
-//function readURL()
-//{
-//	var readerTop = new FileReader();
-//	readerTop.onload = function(e)
-//	{
-//		$('#showTop').attr('src', e.target.result);
-//	};
-//
-//	var readerMid = new FileReader();
-//	readerMid.onload = function(e)
-//	{
-//		$('#showMid').attr('src', e.target.result);
-//	};
-//
-//	var readerBot = new FileReader();
-//	readerBot.onload = function(e)
-//	{
-//		$('#showBot').attr('src', e.target.result);
-//	};
-//
-//	readerTop.readAsDataURL(imgTop.files[0]);
-//	readerMid.readAsDataURL(imgMid.files[0]);
-//	readerBot.readAsDataURL(imgBot.files[0]);
-//}
-function readURL()
+//Initially hide all images 
+$('img').hide();
+
+//Shows files. Prev is for preview id, fil is for file id
+function previewFile(prev, fil)
 {
+	var preview = document.querySelector(prev);
+	var file = document.querySelector(fil).files[0];
 	var reader = new FileReader();
-	reader.onload = function(e)
+
+	reader.addEventListener("load", function() 
 	{
-		file1 = e.target.result;
-		console.log(file1);
+		preview.src = reader.result;
+	}, false);
+
+	if(file)
+	{
+		reader.readAsDataURL(file);
 	}
 }
 
-$("document").ready(function()
+
+$("#btn").click(function()
 {
-	$("#btn").click(function()
-	{
-		var readerBot = new FileReader();
-		readerBot.onload = function (e)
-		{
-			var x = document.createElement("IMG");
-			x.setAttribute("src", e.target.result);
-			x.setAttribute("width", "300");
-			document.body.appendChild(x);
-			readerBot.readAsDataURL(imgBot.files[0]);
-		};
-		var file2 = document.getElementById("showBot");
-		readURL();
-	});
+	//Run previewfile 3 times for each picture
+	previewFile('#showTop', '#imgTop');
+	previewFile('#showMid', '#imgMid');
+	previewFile('#showBot', '#imgBot');
+
+	//Unhides the pictures
+	$('img').show();
 });
+
+
+//1. Load all images into document, one overlapping other. Use for loop for each of the images that are to be uploaded on the same position. New image html element for each file upload
+//2. When button for left or right is clicked, remove or show the image that is corresponding. Account for looping accordingly
+//3. slideIndex counts the current picture index
+//4. Show/hide buttons along with images
+//5. Randomize the initial order?
+var slideIndex = 1;
+
+function plusDivs(n)
+{
+	showDivs(slideIndex += n);
+}
+
+function showDivs(n)
+{
+	var i;
+	var x = document.getElementById("showTop");
+	if(n > x.length)
+	{
+		slideIndex = 1;
+	}
+	if(n < 1)
+	{
+		slideIndex = x.length;
+	}
+	for(i = 0; i < x.length; i++)
+	{
+		x[i].style.display = "none";
+	}
+	x[slideIndex-1].style.display = "block";
+}
